d90cd02fd3279fbc639290a1e96bc65e9aea2a32
digregorio <grehgcl@hotmail.com>
2022-03-27 01:26:36 -0300

    atualização...

---
+++ app.js
@@ -0,0 +1,9 @@
+var Somafun = require('./soma');
+var multfun = require('./multi');
+var subtfun = require('./subtra');    
+
+
+
+console.log(Somafun(1, 2));
+console.log(multfun(10, 2));
+console.log(subtfun(10, 2));
